bb0441c078c9b808fe0545e7df22062bd889e56b
Gonzagadavid <gonzagadaviddev@gmail.com>
2022-01-01 19:40:30 -0300

    adicionado cors para o frontend da aplicação

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { error } from './middlewares';
 import routerRoot from './routers';
 
 dotenv.config();
 
-const { PORT } = process.env;
+const { PORT, FRONTENDAPP } = process.env;
 const started = `started in port ${PORT}`;
+const origin = FRONTENDAPP;
 
 const app = express();
+app.use(cors({ origin }));
 app.use(express.json());
 
 app.use('/', routerRoot);
