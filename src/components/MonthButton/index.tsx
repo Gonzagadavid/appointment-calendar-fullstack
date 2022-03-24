32db8fe58c607b9ba4a8c4803ae70cbc9dc1d3df
gonzagadavid <david.blesses@hotmail.com>
2022-03-24 10:18:05 -0300

    style: estilizado calendario

--- src/components/MonthButton/index.tsx
+++ src/components/MonthButton/index.tsx
@@ -1,16 +1,17 @@
 import React, { useContext } from 'react';
+import { MdArrowBackIosNew, MdArrowForwardIos } from 'react-icons/md';
 import AppContext from '../../contexts/app/AppContext';
 import { DefaultState } from '../../types';
 import { decrementMonth, incrementMonth } from '../../functions';
 
-function MonthButton(props: { text: string}) {
+function MonthButton(props: { text: string }) {
   const appContext = useContext(AppContext) as DefaultState;
   const {
     month, year, setDate,
   } = appContext;
   const { text } = props;
   const checkText = text === 'increment';
-  const icon = checkText ? '>' : '<';
+  const icon = checkText ? <MdArrowForwardIos /> : <MdArrowBackIosNew />;
   const callback = checkText ? incrementMonth : decrementMonth;
   const moveMonth = () => {
     const { newMonth, newYear } = callback(month, year);
