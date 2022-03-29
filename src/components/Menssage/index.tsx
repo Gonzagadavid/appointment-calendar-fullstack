aac972d70e4c23f40b1ef4b1499c13ff4d7f882a
gonzagadavid <david.blesses@hotmail.com>
2022-03-28 22:40:22 -0300

    refator: refatorado para que o componente Messager use o compente ConditionComponent

--- src/components/Menssage/index.tsx
+++ src/components/Menssage/index.tsx
@@ -1,21 +1,20 @@
 import React, { useContext } from 'react';
 import AppContext from '../../contexts/app/AppContext';
 import { DefaultState } from '../../types';
+import ConditionComponent from '../ConditionComponent';
 import './style.css';
 
 function Message() {
   const appContext = useContext(AppContext);
   const { message, setMessage } = appContext as DefaultState;
 
-  if (!message) return null;
-
   return (
-    <div className="Message">
+    <ConditionComponent condition={!!message} className="Message">
       <div className="message-container">
         <h2>{message}</h2>
         <button type="button" onClick={() => setMessage('')}>OK</button>
       </div>
-    </div>
+    </ConditionComponent>
   );
 }
 
