663a873b9a47a39e71e73cd91fda060f34cdc8f5
gonzagadavid <david.blesses@hotmail.com>
2022-04-21 18:29:56 -0300

    test: desenvolvido testes unitários para os métodos putTask e deleteTask  da classe TasksController

--- src/tests/unit-tests/controllers/TasksControllers.test.ts
+++ src/tests/unit-tests/controllers/TasksControllers.test.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { TASK_MODIFY, TASK_REMOVED } from '../../../constants/strings';
 import TasksController from '../../../controllers/TasksController';
 import { INTERNAL_ERROR } from '../../../errors';
 import TasksService from '../../../services/TasksService';
@@ -94,6 +95,44 @@ describe('verifica o funcionamento do métodos da classe TaskController', () => 
     service.insertTask = jest.fn().mockRejectedValue(INTERNAL_ERROR);
     const controller = new TasksController(service);
     await controller.postTask(req, res, next);
+
+    expect(next).toBeCalledWith(INTERNAL_ERROR);
+  });
+
+  it('verifica o funcionamento do método putTask', async () => {
+    const service = new TasksService();
+    service.updateTask = jest.fn().mockResolvedValue(task);
+    const controller = new TasksController(service);
+    await controller.putTask(req, res, next);
+
+    expect(res.status).toBeCalledWith(202);
+    expect(json).toBeCalledWith({ message: TASK_MODIFY });
+  });
+
+  it('verifica se ao ocorrer um erro no método putTask é passado para o next', async () => {
+    const service = new TasksService();
+    service.updateTask = jest.fn().mockRejectedValue(INTERNAL_ERROR);
+    const controller = new TasksController(service);
+    await controller.putTask(req, res, next);
+
+    expect(next).toBeCalledWith(INTERNAL_ERROR);
+  });
+
+  it('verifica o funcionamento do método deleteTask', async () => {
+    const service = new TasksService();
+    service.removeTask = jest.fn().mockResolvedValue(task);
+    const controller = new TasksController(service);
+    await controller.deleteTask(req, res, next);
+
+    expect(res.status).toBeCalledWith(202);
+    expect(json).toBeCalledWith({ message: TASK_REMOVED });
+  });
+
+  it('verifica se ao ocorrer um erro no método deleteTask é passado para o next', async () => {
+    const service = new TasksService();
+    service.removeTask = jest.fn().mockRejectedValue(INTERNAL_ERROR);
+    const controller = new TasksController(service);
+    await controller.deleteTask(req, res, next);
 
     expect(next).toBeCalledWith(INTERNAL_ERROR);
   });
